aca6e77e79c119c516e9d638812165307a93cec2
Bruno Cruz de Barros <bcbosss@gmail.com>
2022-10-27 21:00:50 -0300

    Introduction complete and sound errors fixed

---
+++ Aventura do Pantanal/Assets/Scripts/Narrative scripts/Introduction.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introduction : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip[] soundsOfIntroduction;
+
+    [SerializeField]
+    private int currentAudioClip = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        goToNextAudioClip();
+        goToNextScene();
+    }
+
+    private void goToNextAudioClip()
+    {
+        if(!audioSource.isPlaying && currentAudioClip < soundsOfIntroduction.Length)
+        {
+            audioSource.PlayOneShot(soundsOfIntroduction[currentAudioClip]);
+            currentAudioClip++;
+        }
+    }
+
+    private void goToNextScene()
+    {
+        if(!audioSource.isPlaying && currentAudioClip >= soundsOfIntroduction.Length)
+        {
+            ChangeSceneManager.GoToSomeScene("2 Tutorial");
+        }
+    }
+}
